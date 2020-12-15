87bc8e162f9b68097c2651b2618ccd07ab31c928
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2020-12-15 10:41:33 +0530

    Update command-exe.py

--- command-exe.py
+++ command-exe.py
@@ -1,14 +1,15 @@
+#before running this file make sure that you had the speech_recognition and subprocess modules are installed in your system
 import speech_recognition as speech
 import subprocess
-rec = speech.Recognizer()
+rec = speech.Recognizer()   
 with speech.Microphone() as source:
     print("speak your command :-)")
-    aud = rec.listen(source)
+    aud = rec.listen(source) #here your voice is stored in a variable
     try:
         rec.adjust_for_ambient_noise(source, duration=0.2) 
-        t = rec.recognize_google(aud,language="en")
+        t = rec.recognize_google(aud,language="en")         #here your languageof your voice is recognized 
         t = list(t.split(" "))
-        if("Notepad" or "notepad" in t):
+        if("Notepad" or "notepad" in t):            #here your applications are opend based on your commands
             subprocess.call('notepad.exe')
         elif("Calculator" in t):
             subprocess.call('calc.exe')
@@ -18,6 +19,6 @@ with speech.Microphone() as source:
             subprocess.call('C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe')
         elif("Firefox" or "Mozilla" or "firefox" in t):
             subprocess.call("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe")
-        print(t)
+        print(' '.join(t))           #here your voice command is printed 
     except:
         print("problem occured")
