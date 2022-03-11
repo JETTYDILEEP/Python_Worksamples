8fe04ec3bdcf5a77ef33ebc2de9fcd68fbb5db38
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2022-03-11 18:09:18 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -7,13 +7,13 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls() 
   
 # Authentication 
-s.login("dileepchinna333@gmail.com", "Dileep!@2309") 
+s.login("user mailid", "password") 
   
 # message to be sent 
 message = "hi hello"
   
 # sending the mail 
-s.sendmail("dileepchinna333@gmail.com", "jettydileepreddy@gmail.com", message) 
+s.sendmail("user mail id", "sender mail id", message) 
   
 # terminating the session 
 s.quit() 
