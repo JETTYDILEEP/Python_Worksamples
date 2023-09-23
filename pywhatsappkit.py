f17249d66c22a2ff9c0fd869c31436bf4d34425d
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2023-09-23 21:44:43 +0530

    worksamples from vs code

---
+++ pywhatsappkit.py
@@ -0,0 +1,8 @@
+import pywhatkit
+
+#whats_app_number = ''
+'''pywhatkit.sendwhatmsg(whats_app_number,
+					"Geeks For Geeks!",
+					22, 22, 15, True, 2)
+'''
+pywhatkit.sendwhatmsg_to_group_instantly("", "Hey All Please avoid this message just checking!")
